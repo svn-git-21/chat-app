d402c79c5efb03a16df50fce5ff50320defa5a1b
Souvik Maji <souvik2015.svk@gmail.com>
2025-05-18 13:49:56 +0530

    tailwindcss fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/auth.route.js';
-
+import messageRoutes from "./routes/message.route.js";
+ 
 dotenv.config()
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT : " + PORT);
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/user",protectRoute, getUsersForSidebar)
+router.get("/:id",protectRoute, getMessages)
+router.post("/send/:id", protectRoute, sendMessage)
+export default router;
