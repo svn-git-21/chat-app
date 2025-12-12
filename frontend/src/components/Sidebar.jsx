fe54849c917ebc7f3415dbac882888b20e015792
Maji <2284128@cognizant.com>
2025-12-12 20:37:56 +0530

    chatContainerStart

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
+
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
 
-  const onlineUsers = [];
+  const { onlineUsers }  = useAuthStore();
 
   useEffect(() => {
     getUsers();
